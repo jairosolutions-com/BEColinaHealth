f30769b4ec01149d743800d5ac7c504c69eb2172
kntjhnllc <kentjohnliloc@gmail.com>
2024-02-29 15:03:33 +0800

    #colina88-create,update-ual

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,20 +9,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Any, FindManyOptions, ILike, IsNull, Like, Repository } from 'typeorm';
 import { CreateUserInput } from './dto/create-user.input';
 import { Users } from './entities/user.entity';
-import { IdService } from 'services/uuid/id.service'; // Import IdService
+import { IdService } from 'services/uuid/id.service'; // Import idService
 import * as bcrypt from 'bcrypt';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UserAccessLevel } from 'src/user_access_level/entities/user_access_level.entity';
+import { Role } from 'src/role/entities/role.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private readonly usersRepository: Repository<Users>,
+    @InjectRepository(Role) private readonly roleRepository: Repository<Role>,
+    @InjectRepository(UserAccessLevel)
+    private readonly ualRepository: Repository<Role>,
     private readonly idService: IdService,
   ) {}
 
   async getUserById(id: number): Promise<Users | undefined> {
-    return this.usersRepository.findOne({ where: { Id: id } });
+    return this.usersRepository.findOne({ where: { id: id } });
   }
 
   async getAllUsers(
@@ -122,7 +127,7 @@ export class UsersService {
   async createUser(createUserInput: CreateUserInput): Promise<Users> {
     // Check if email already exists
     const existingUser = await this.usersRepository.findOne({
-      where: { email: ILike(`%${createUserInput.email}%`) }, // Use ILike for case-insensitive comparison
+      where: { email: ILike(`%${createUserInput.email}%`) },
     });
 
     if (existingUser) {
@@ -130,7 +135,7 @@ export class UsersService {
     }
 
     const newUser = new Users();
-    newUser.uuid = this.idService.generateRandomUUID('UID-');
+    newUser.uuid = this.idService.generateRandomUUID('Uid-');
     newUser.email = createUserInput.email;
     newUser.fName = createUserInput.fName;
     newUser.lName = createUserInput.lName;
@@ -142,18 +147,61 @@ export class UsersService {
 
     newUser.password = await this.hashPassword(createUserInput.password);
 
-    return this.usersRepository.save(newUser);
+    // Save the new user to the database
+    const savedUser = await this.usersRepository.save(newUser);
+
+    // Now, create a new user access level record
+    // Assuming Role with id 3 is the default role
+    const defaultRole = await this.roleRepository.findOne({ where: { id: 3 } });
+    if (!defaultRole) {
+      throw new Error('Default role not found');
+    }
+
+    const newUserAccessLevel = new UserAccessLevel();
+    newUserAccessLevel.users = savedUser; // Assign the user to the user property
+    newUserAccessLevel.role = defaultRole; // Assign the fetched role to the role property
+
+    // Save the new user access level record to the database
+    await this.ualRepository.save(newUserAccessLevel);
+
+    return savedUser;
   }
+
+  // async createUser(createUserInput: CreateUserInput): Promise<Users> {
+  //   // Check if email already exists
+  //   const existingUser = await this.usersRepository.findOne({
+  //     where: { email: ILike(`%${createUserInput.email}%`) }, // Use ILike for case-insensitive comparison
+  //   });
+
+  //   if (existingUser) {
+  //     throw new HttpException('Email already exists', HttpStatus.CONFLICT);
+  //   }
+
+  //   const newUser = new Users();
+  //   newUser.uuid = this.idService.generateRandomUUid('Uid-');
+  //   newUser.email = createUserInput.email;
+  //   newUser.fName = createUserInput.fName;
+  //   newUser.lName = createUserInput.lName;
+  //   newUser.status = createUserInput.status;
+
+  //   if (!createUserInput.password) {
+  //     throw new HttpException('Password is required', HttpStatus.BAD_REQUEST);
+  //   }
+
+  //   newUser.password = await this.hashPassword(createUserInput.password);
+
+  //   return this.usersRepository.save(newUser);
+  // }
 
   // UPDATE
 
   async updateUser(
-    Id: number,
+    id: number,
     updateUserInput: UpdateUserInput,
   ): Promise<Users> {
-    const user = await this.usersRepository.findOne({ where: { Id } });
+    const user = await this.usersRepository.findOne({ where: { id } });
     if (!user) {
-      throw new Error(`User with ID ${Id} not found`);
+      throw new Error(`User with id ${id} not found`);
     }
 
     // Update user properties if provided in updateUserInput
@@ -177,11 +225,11 @@ export class UsersService {
     return this.usersRepository.save(user);
   }
 
-  async softDeleteUser(Id: number): Promise<Users> {
+  async softDeleteUser(id: number): Promise<Users> {
     // Check if the user exists
-    const user = await this.usersRepository.findOne({ where: { Id: Id } });
+    const user = await this.usersRepository.findOne({ where: { id: id } });
     if (!user) {
-      throw new NotFoundException(`User with ID ${Id} not found`);
+      throw new NotFoundException(`User with id ${id} not found`);
     }
 
     // Save and return the updated user
