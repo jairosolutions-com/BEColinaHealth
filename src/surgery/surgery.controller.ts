62e3a156efd41e8094d4274d768e458a10e16f7a
kntjhnllc <kentjohnliloc@gmail.com>
2024-03-07 09:04:31 +0800

    for merging branch to develop

--- src/surgery/surgery.controller.ts
+++ src/surgery/surgery.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SurgeryService } from './surgery.service';
 import { CreateSurgeryDto } from './dto/create-surgery.dto';
 import { UpdateSurgeryDto } from './dto/update-surgery.dto';
 
 @Controller('surgery')
-export class SurgeryController {
-  constructor(private readonly surgeryService: SurgeryService) {}
-
-  @Post()
-  create(@Body() createSurgeryDto: CreateSurgeryDto) {
-    return this.surgeryService.create(createSurgeryDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.surgeryService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.surgeryService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSurgeryDto: UpdateSurgeryDto) {
-    return this.surgeryService.update(+id, updateSurgeryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.surgeryService.remove(+id);
-  }
-}
+export class SurgeryController {}
