81b18ccc3366a292a70b4a0ab375a15980ba305a
kntjhnllc <kentjohnliloc@gmail.com>
2024-03-07 09:03:54 +0800

    for merging branch to develop

--- src/surgery/surgery.service.ts
+++ src/surgery/surgery.service.ts
@@ -3,24 +3,4 @@ import { CreateSurgeryDto } from './dto/create-surgery.dto';
 import { UpdateSurgeryDto } from './dto/update-surgery.dto';
 
 @Injectable()
-export class SurgeryService {
-  create(createSurgeryDto: CreateSurgeryDto) {
-    return 'This action adds a new surgery';
-  }
-
-  findAll() {
-    return `This action returns all surgery`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} surgery`;
-  }
-
-  update(id: number, updateSurgeryDto: UpdateSurgeryDto) {
-    return `This action updates a #${id} surgery`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} surgery`;
-  }
-}
+export class SurgeryService {}
