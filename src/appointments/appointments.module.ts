cc36ac70e49fc2258decdd378fe5efdef1708c22
nachocheez <cheskapg@gmail.com>
2024-03-11 12:58:00 +0800

    services update

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsResolver } from './appointments.resolver';
 import { AppointmentsController } from './appointments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointments } from './entities/appointments.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Appointments])],
   providers: [AppointmentsResolver, AppointmentsService],
   controllers: [AppointmentsController],
 })
--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAppointmentsInput } from './dto/create-appointments.input';
 import { UpdateAppointmentsInput } from './dto/update-appointments.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IdService } from 'services/uuid/id.service';
+import { Repository } from 'typeorm';
+import { Appointments } from './entities/appointments.entity';
 
 @Injectable()
 export class AppointmentsService {
-  create(createAppointmentsInput: CreateAppointmentsInput) {
-    return 'This action adds a new appointments';
+  constructor(
+    @InjectRepository(Appointments)
+    private appointmentsRepository: Repository<Appointments>,   
+    private idService: IdService, // Inject the IdService
+  ) {}
+  async createAppointments(input: CreateAppointmentsInput): Promise<Appointments> {
+    const existingLowercaseboth = await this.appointmentsRepository.findOne({
+      where: {
+       
+        patientId: (input.patientId)
+      },
+    });
+    if (existingLowercaseboth) {
+      throw new ConflictException('Appointment already exists.');
+    }
+    const newAppointments = new Appointments();
+    const uuidPrefix = 'APT-'; // Customize prefix as needed
+    const uuid = this.idService.generateRandomUUID(uuidPrefix);
+    newAppointments.uuid = uuid;
+    Object.assign(newAppointments, input);
+    return this.appointmentsRepository.save(newAppointments);
   }
-
-  findAll() {
-    return `This action returns all appointments`;
+  async getAllAppointmentsByPatient(patientId: number, page: number = 1, sortBy: string = 'appointmentDate', sortOrder: 'ASC' | 'DESC' = 'ASC', perPage: number = 5): Promise<{ data: Appointments[], totalPages: number, currentPage: number, totalCount }> {
+    const skip = (page - 1) * perPage;
+    const totalPatientAppointments = await this.appointmentsRepository.count({
+      where: { patientId },
+      skip: skip,
+      take: perPage,
+    });
+    const totalPages = Math.ceil(totalPatientAppointments / perPage);
+    const appointmentsList = await this.appointmentsRepository.find({
+      where: { patientId },
+      skip: skip,
+      take: perPage,
+    });
+    return {
+      data: appointmentsList,
+      totalPages: totalPages,
+      currentPage: page,
+      totalCount: totalPatientAppointments
+    };
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} appointments`;
+  async getAllAppointments(): Promise<Appointments[]> {
+    return this.appointmentsRepository.find();
   }
-
-  update(id: number, updateAppointmentsInput: UpdateAppointmentsInput) {
-    return `This action updates a #${id} appointments`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} appointments`;
+  async updateAppointments(id: number, updateAppointmentsInput: UpdateAppointmentsInput): Promise<Appointments> {
+    const appointments = await this.appointmentsRepository.findOne(id);
+    Object.assign(appointments, updateAppointmentsInput);
+    return this.appointmentsRepository.save(appointments);
   }
 }
